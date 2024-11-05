de4d64d86b862b73dd76a248ea63ce00fe11c9a0
Lethabo Penniston <ST10302369@vcconnect.edu.za>
2024-11-05 13:42:47 +0200

    Added azure storage services, blob, queue and file share

--- Program.cs
+++ Program.cs
@@ -1,15 +1,39 @@
 using CloudPart3.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using CloudPart3.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Access the configuration object
+var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Register BlobService with configuration
+builder.Services.AddSingleton(new BlobService(configuration.GetConnectionString("AzureStorage")));
+
+
+// Register QueueService with configuration
+builder.Services.AddSingleton<QueueService>(sp =>
+{
+    var connectionString = configuration.GetConnectionString("AzureStorage");
+    return new QueueService(connectionString); // Pass connection string only
+});
+
+// Register FileShareService with configuration
+builder.Services.AddSingleton<AzureFileShareService>(sp =>
+{
+    var connectionString = configuration.GetConnectionString("AzureStorage");
+    return new AzureFileShareService(connectionString, "contractsshare");
+});
+
+
 //Adding DB Context builder services with options
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
            options.UseSqlServer(builder.Configuration.GetConnectionString("ABCRetailersDEV")));
+
 //Added service for Authorization for Role based Access
 builder.Services.AddDefaultIdentity<IdentityUser>().AddDefaultTokenProviders()
                .AddRoles<IdentityRole>()
@@ -32,7 +56,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-app.MapRazorPages(); 
+
+app.MapRazorPages();
+
 
 app.MapControllerRoute(
     name: "default",
